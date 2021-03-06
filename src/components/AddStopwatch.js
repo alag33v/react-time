6606f220432a589f8e01e5020ced4e3205f09f28
alageev <alageev5610@gmail.com>
2021-03-06 19:08:40 +0200

    Add new stopwatch on click

--- src/components/AddStopwatch.js
+++ src/components/AddStopwatch.js
@@ -1,9 +1,25 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import moment from 'moment';
+import { v4 as uuid } from 'uuid';
+import { addNewStopwatch } from '../redux/ducks/timeDucks';
 
 const AddStopwatch = () => {
   let [trackName, setTrackName] = useState('');
+  const dispatch = useDispatch();
 
-  const handleSubmit = () => {};
+  const handleSubmit = e => {
+    e.preventDefault();
+    const newObj = {
+      id: uuid(),
+      title: trackName || moment().format('Do MMMM YYYY, H:mm:ss a'),
+      savedTime: 0,
+      isActive: true
+    };
+
+    dispatch(addNewStopwatch(newObj));
+    setTrackName('');
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -14,6 +30,7 @@ const AddStopwatch = () => {
           onChange={e => setTrackName(e.target.value)}
           value={trackName}
         />
+        <button type='submit'>Add</button>
       </div>
     </form>
   );
